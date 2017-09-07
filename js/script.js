07d96ca3a8c1922e02f903a485f809b7d8d5bd34
DmitriyBaguln1k <lerkondim@mail.com>
2017-09-07 14:06:17 +0700

    Revert "Revert "Главная Проверка""
    This reverts commit e7dac99c41520c50bebfee859833c6ccad63591c.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var link = document.querySelector(".search-hotels-btn");
 var modal = document.querySelector(".popup");
-modal.classList.add("modal-content-closed")
+modal.classList.add("modal-content-opened")
 link.addEventListener("click", function(event){
 event.preventDefault();
-  modal.classList.toggle("modal-content-opened")
+  modal.classList.toggle("modal-content-closed")
 });
